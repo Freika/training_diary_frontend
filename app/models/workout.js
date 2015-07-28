ef1c1d1dd71dd24566d0644e63ad3fcc5b1a794e
Freika <iamfrey@gmail.com>
2015-07-29 02:01:17 +0600

    Add active model adapter and workout index.

--- app/models/workout.js
+++ app/models/workout.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   trainedAt: DS.attr('date'),
   day: DS.attr('number'),
-  description: DS.attr()
+  description: DS.attr('string')
 });
