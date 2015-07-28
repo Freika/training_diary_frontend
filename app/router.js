07ee4423509b375360004af1f0c43ebf3b0f2343
Freika <iamfrey@gmail.com>
2015-07-29 02:13:36 +0600

    Fix listing in index.

--- app/router.js
+++ app/router.js
@@ -6,7 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('workouts', function() {});
+  this.route('workouts', function() {
+
+  });
 });
 
 export default Router;
