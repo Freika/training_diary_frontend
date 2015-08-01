6d065eef046d7de93decc6e624c1ffb3e26e800a
Freika <iamfrey@gmail.com>
2015-08-01 19:47:00 +0600

    Add broken workout deleting.

--- app/routes/workouts.js
+++ app/routes/workouts.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    save() {
+      return true;
+    },
+    cancel() {
+      return true;
+    },
+    delete: function(workout) {
+      workout.destroyRecord().then(() => {
+        this.transitionTo('workouts.index');
+      });
+    }
+  }
 });
